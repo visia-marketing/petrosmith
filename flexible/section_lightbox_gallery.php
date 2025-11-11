13c173e690ec4a0f8fb4763a2b1b55c1102820c9
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-11-11 14:24:04 -0500

    updated colors of counter and caption

--- flexible/section_lightbox_gallery.php
+++ flexible/section_lightbox_gallery.php
@@ -12,7 +12,8 @@ $lightbox_gallery = get_sub_field('lightbox_gallery');
             <?php foreach( $lightbox_gallery as $image ): ?>
                 <div class="column small-12 medium-6 large-4">
                     <a href="<?php echo wp_get_attachment_image_src( $image['image'], 'full' )[0]; ?>" data-lightbox="gallery" class="lightbox-anchor">
-                        <img src="<?php echo wp_get_attachment_image_src( $image['image'], 'thumbnail' )[0]; ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        <img src="<?php echo wp_get_attachment_image_src( $image['image'], 'medium' )[0]; ?>" alt="<?php echo esc_attr($image['alt']); ?>" data-caption="<?php echo $image['caption'];?> "/>
+                        <span class="caption-text"><?php echo $image['caption'];?></span>
                     </a>
                 </div>
             <?php endforeach; ?>
@@ -27,6 +28,12 @@ $lightbox_gallery = get_sub_field('lightbox_gallery');
 
             var options = {
                 overlayOpacity: 1,
+                showCaptions: true,
+                captionSelector: 'img',
+                captionType: 'attr',
+                captionsData: 'data-caption',
+                widthRatio: 0.6,
+
             };
 
             new SimpleLightbox('[data-lightbox="gallery"]',  options );
