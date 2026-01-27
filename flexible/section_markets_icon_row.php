4bf458cc589af8089d346f05edd2bac122c5d2f2
Randal Pope <randalpope@Mac.hsd1.pa.comcast.net>
2026-01-27 15:34:04 -0500

    added slider to market icon grid

--- flexible/section_markets_icon_row.php
+++ flexible/section_markets_icon_row.php
@@ -1,15 +1,34 @@
 <?php 
-
+$rand_id = 'markets-icon-row-' . wp_generate_uuid4();
 $markets = get_sub_field('choose_markets');
 
+$slider = get_sub_field('display');
+$per_row = get_sub_field('row_display');
 $class = '';  
 
-if(  count($markets) > 8 ){
-    $class = 'icon-row--more-than-8';
+
+if( $slider == 1 ){
+    $class .= ' display-slider';  
+}else{
+    $class .= ' display-grid'; 
+}
+
+
+
+if(  count($markets) == 7 && $per_row == 8 ){
+    $per_row = 7;
+}
+
+if(  count($markets) == 5 && $per_row == 6 ){
+    $per_row = 5;
+}
+
+if(  count($markets) == 3 && $per_row == 4 ){
+    $per_row = 3;
 }
 
 ?>
-<div class="row">
+<div class="row flexible-market-icons" id="<?php echo $rand_id; ?>">
     <div class="columns small-12">
         <div class="icon-row <?php echo $class; ?>">
         <?php foreach($markets as $m_id ): ?>
@@ -34,3 +53,44 @@ if(  count($markets) > 8 ){
     </div>
     </div>
 </div>
+
+<style>
+    #<?php echo $rand_id; ?> .icon-row.display-grid {
+        display: grid;
+        grid-template-columns: repeat(<?php echo esc_html( $per_row ); ?>, 1fr);
+        gap: 20px;
+    }
+
+    #<?php echo $rand_id; ?> .icon-row.display-slider {
+        display: flex;
+    }
+
+    .flexible-market-icons .slick-prev,
+    .flexible-market-icons .slick-next {
+        height: auto;
+    }
+
+    .flexible-market-icons .slick-prev:before,
+    .flexible-market-icons .slick-next:before {
+        display: none;
+    }
+
+</style>
+
+<?php if( $slider == 1 ): ?>
+
+<script>
+    jQuery(function($) {
+        $('#<?php echo $rand_id;?> .icon-row').slick({
+            infinite: true,
+            slidesToShow: <?php echo $per_row; ?>,
+            slidesToScroll: 1,
+            arrows: true,
+            dots: false,
+            prevArrow: '<button type="button" class="slick-prev cards-next"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="41" viewBox="0 0 23 41" fill="none"> <path d="M21.123 1.5L2.12129 20.5018L21.123 39.5035" stroke="#062F6E" stroke-width="3" stroke-linecap="round"/></svg></button>',
+            nextArrow: '<button type="button" class="slick-next cards-prev"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="41" viewBox="0 0 23 41" fill="none"><path d="M1.5 39.5034L20.5018 20.5017L1.5 1.4999" stroke="#062F6E" stroke-width="3" stroke-linecap="round"/></svg></button>',
+        });
+    });
+</script>
+
+<?php endif; ?>
