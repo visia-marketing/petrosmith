d7805a4c197842f075d59e9d53cb6fe109f8480c
Randal Pope <randalpope@mac.mynetworksettings.com>
2026-02-05 10:51:04 -0500

    added responsive styles for animated numbers

---
+++ flexible/section_featured_callout.php
@@ -0,0 +1,34 @@
+<?php
+
+$design = get_sub_field('callout_design');
+$background_color = $design['background'];
+$image = $design['callout_image'];
+
+$content = get_sub_field('callout_content');
+
+?>
+
+
+<div class="featured-callout">
+    <div class="row">
+
+        <div class="column small-10 large-10 small-offset-1 callout-column">
+
+            <div class="callout-content callout-background--<?php echo $background_color; ?>">
+
+                <?php if( $image ): ?>
+                   <?php echo wp_get_attachment_image( $image, 'medium' ); ?>
+                <?php endif; ?>
+
+                <?php if( $content ): ?>
+                   <p><?php echo $content; ?><p>
+                <?php endif; ?>
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+</div>
